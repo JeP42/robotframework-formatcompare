a05d9cc2feb81978b60d46d14e13c60626205a97
jep42 <jep0042@gmail.com>
2016-12-07 00:16:41 +0100

    formatting

--- formatcompare/src/main/java/com/github/jep42/formatcompare/elementcomparator/api/ElementComparator.java
+++ formatcompare/src/main/java/com/github/jep42/formatcompare/elementcomparator/api/ElementComparator.java
@@ -2,7 +2,6 @@ package com.github.jep42.formatcompare.elementcomparator.api;
 
 import com.github.jep42.formatcompare.fieldmapper.api.FieldMapping;
 import com.github.jep42.formatcompare.formathandler.api.FormatHandler;
-import com.github.jep42.formatcompare.util.FormatComparatorException;
 
 public interface ElementComparator {
 
